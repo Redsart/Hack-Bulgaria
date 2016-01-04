c4ab3925a6f3f2cc64d90227f4483fb723134e4a
Redsart <redsartvimes@yahoo.com>
2016-01-04 14:59:46 +0200

    Added CashDesk Problem

---
+++ Week3/02.Thursday/CashDeskProblem/CashDesk/Bill.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CashDesk
+{
+    public class Bill
+    {
+        private int amount;
+        
+        public int Amount
+        {
+            get
+            {
+                return this.amount;
+            }
+            set
+            {
+                this.amount = value;
+            }
+        }
+
+        public Bill(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            var ri = new RegionInfo(System.Threading.Thread.CurrentThread.CurrentCulture.LCID);
+            return String.Format("{0} {1} Bill",this.amount,ri.ISOCurrencySymbol);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Bill obj2 = obj as Bill;
+            if (obj2 is Bill)
+            {
+                if (this.amount==obj2.amount)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator ==(Bill a, Bill b)
+        {
+            if (a.amount==b.amount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Bill a, Bill b)
+        {
+            return !(a == b);
+        }
+
+        public static explicit operator int(Bill bill)
+        {
+            return (int)bill;
+        }
+    }
+}
---
+++ Week3/02.Thursday/CashDeskProblem/CashDeskApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashDesk;
+
+namespace CashDeskApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var a = new Bill(10);
+            var b = new Bill(5);
+            var c = new Bill(10);
+
+            Console.WriteLine(a);
+        }
+    }
+}
