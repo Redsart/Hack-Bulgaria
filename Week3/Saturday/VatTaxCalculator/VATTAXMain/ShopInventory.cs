fa5a06831e06f5fe336f200166d302afb5322689
Redsart <redsartvimes@yahoo.com>
2016-01-12 13:53:28 +0200

    Added VAT Tax Calculator Problem

---
+++ Week3/Saturday/VatTaxCalculator/VATTAXMain/ShopInventory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VATTAXMain
+{
+    public class ShopInventory
+    {
+        private Dictionary<Product, int> products = new Dictionary<Product, int>();
+
+        public ShopInventory(Product newProduct)
+        {
+            if (products.ContainsKey(newProduct))
+            {
+                products[newProduct] += 1;
+            }
+            else
+            {
+                products.Add(newProduct, 1);
+            }
+        }
+
+        public double Audit()
+        {
+            double total = 0;
+            foreach (var item in products)
+            {
+                total += (item.Key.PrizeWithTax * item.Key.Quantity);
+            }
+            return total;
+        }
+
+        public void RequestOrder(Order order)
+        {
+            foreach (var item in products)
+            {
+                if (item.Key.Id==order.ProductId)
+                {
+                    if (item.Key.Quantity >= order.Quantity)
+                    {
+                        double result = 0;
+                        result = item.Key.PrizeWithTax * order.Quantity;
+                        Console.WriteLine("The order will cost you: {0}",result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough quantity.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("We dont have the product in our inventory.");
+                }
+            }
+        }
+    }
+}
---
+++ Week3/Saturday/VatTaxCalculator/VATTAXMain/VatTaxCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VATTAXMain
+{
+    class VatTaxCalculator
+    {
+        static void Main(string[] args)
+        {
+            CountryVatTax obj = new CountryVatTax(1,20,false);
+            CountryVatTax secondObj = new CountryVatTax(2, 15, true);
+            List<CountryVatTax> countries = new List<CountryVatTax> { obj, secondObj};
+            Calculator calculator = new Calculator(countries);
+            calculator.CalculateTax(4);
+            calculator.CalculateTax(4, 2);
+            Product product = new Product(4,2,"Storm",4,2,countries);
+            Product secondProduct = new Product(8, 1, "Wind", 5, 1, countries);
+            ShopInventory shopInventory = new ShopInventory(product);
+            Console.WriteLine("If all products of that type are sold out, the profil you will gain is = {0}", shopInventory.Audit());
+            Order order = new Order(1, 3);
+            shopInventory.RequestOrder(order);
+        }
+    }
+}
