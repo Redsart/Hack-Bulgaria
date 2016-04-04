62f2acde815cf26f6e7a4b201c755627b7c5237a
Redsart <redsartvimes@yahoo.com>
2016-04-05 00:25:15 +0300

    Added Delegates and Events solution.

---
+++ Week6/02.DelegatesAndEvents/Agregates/AgregatesApp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregates
+{
+    class AgregatesApp
+    {
+        public delegate decimal AggregationDelegate(List<int> numbers);
+
+        //public delegate void AnotherAggregationDelegate(List<int> numbers);
+
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 2, 3, 14, 12, 19, 6, 4 };
+
+            AggregationDelegate myDelegate = CalculateAverage;
+
+            // Test void delegates
+
+            //AnotherAggregationDelegate mySecondDelegate = Min;
+            //mySecondDelegate += Max;
+            //mySecondDelegate += Sum;
+            //mySecondDelegate += Average;
+
+            Console.WriteLine("Average of numbers is {0}",AggregateCollection(numbers,myDelegate));
+        }
+
+        public static decimal AggregateCollection(List<int> original, AggregationDelegate myDelegate)
+        {
+            return myDelegate(original);
+        }
+
+        public static void Sum(List<int> numbers)
+        {
+            var sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine("Sum of numbers is {0}",sum);
+        }
+
+        public static void Average(List<int> numbers)
+        {
+            var sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            var average = sum / numbers.Count;
+            Console.WriteLine("Average of numbers is {0}",average);
+        }
+
+        public static decimal CalculateAverage(List<int> numbers)
+        {
+            var sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            var average = sum / numbers.Count;
+            return average;
+        }
+
+        public static void Min(List<int> numbers)
+        {
+            var min = int.MaxValue;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine("Min element is {0}",min);
+        }
+
+        public static void Max(List<int> numbers)
+        {
+            var max = int.MinValue;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine("Max element is {0}",max);
+        }
+    }
+}
---
+++ Week6/02.DelegatesAndEvents/Filters/FiltersApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filters
+{
+    class FiltersApp
+    {
+        public delegate bool FilterDelegate(int number);
+
+        static void Main(string[] args)
+        {
+            //Program filters numbers greater than 5
+
+            FilterDelegate filter = IsInRange;
+
+            var numbers = new List<int> { 0, 2, 14, 12, 11, 4, -2, 6, 10 };
+
+            numbers = FilterCollection(numbers, filter);
+
+            var result = String.Join(", ", numbers);
+
+            Console.WriteLine(result);
+        }
+
+        public static List<int> FilterCollection(List<int> original, FilterDelegate filter)
+        {
+            for (int i = 0; i < original.Count; i++)
+            {
+                if (!filter(original[i]))
+                {
+                    original.Remove(original[i]);
+                    i--;
+                }
+            }
+            return original;
+        }
+
+        public static bool IsInRange(int number)
+        {
+            if (number > 5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
