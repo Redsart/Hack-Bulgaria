1857c17589cce8f21e9a28c44b9a37fe5cfda85e
Redsart <redsartvimes@yahoo.com>
2016-04-08 10:37:43 +0300

    Added Network receive buffer.

---
+++ Week6/02.DelegatesAndEvents/Network/MessageArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class MessageArgs : EventArgs
+    {
+        private string message;
+        public MessageArgs(string message)
+        {
+            this.message = message;
+        }
+
+        public string GetMessage
+        {
+            get
+            {
+                return this.message;
+            }
+        }
+    }
+}
---
+++ Week6/02.DelegatesAndEvents/Network/NetworkRecieveBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    class NetworkRecieveBuffer
+    {
+        static void Main(string[] args)
+        {
+            string message = "# Hack Bulgaria #";
+            ReceiveBuffer buffer = new ReceiveBuffer();
+
+            // Attach method as a listener
+            buffer.MessageReceived += OnMessageRecived;
+            
+            // Add byte data to buffer
+            buffer.BytesReceived(PacketGenerator.ConvertToByteArray(message));
+            
+            string anotherMessage = "Programming is awesome!";
+            // Add new byte data to buffer
+            buffer.BytesReceived(PacketGenerator.ConvertToByteArray(anotherMessage));
+        }
+
+        private static void OnMessageRecived(object sender, MessageArgs ev) 
+        {
+            Console.WriteLine(ev.GetMessage);
+        }
+    }
+}
