82959862f7808b51dbedc2e3829d95ac00500490
Redsart <redsartvimes@yahoo.com>
2016-04-10 23:38:33 +0300

    Added Anonymous types and nested classes.

---
+++ Week6/03.Static-Partial-Anonymous-Types/AnonymousTypesAndNestedClasses/MagazineAndBookSorter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypesAndNestedClasses
+{
+    public static class MagazineAndBookSorter
+    {
+        public class Edition
+        {
+            public string EditionName { get; set; }
+            public int Order { get; set; }
+        }
+
+        public static List<string> Sort(List<Book> books, List<Magazine> magazines)
+        {
+            var edition = new List<Edition>();
+            foreach (var book in books)
+            {
+                var editionItem = new Edition() { EditionName = book.Name, Order = book.Id };
+                edition.Add(editionItem);
+            }
+
+            foreach (var magazine in magazines)
+            {
+                var editionItem = new Edition() { EditionName = magazine.Title, Order = magazine.ISBN };
+                edition.Add(editionItem);
+            }
+
+            var stringList = new List<string>();
+            var orderEditions = edition.OrderBy(x => x.EditionName).ToList();
+
+            while (true)
+            {
+                if (orderEditions.Count < 1)
+                {
+                    break;
+                }
+
+                var tempEditionList = new List<Edition>();
+                var currentItem = orderEditions.FirstOrDefault();
+                for (int i = 0; i < orderEditions.Count; i++)
+                {
+                    if (orderEditions[i].EditionName == currentItem.EditionName)
+                    {
+                        tempEditionList.Add(orderEditions[i]);
+                        orderEditions.Remove(orderEditions[i]);
+                        i--;
+                    }
+                }
+                var orderedTemp = tempEditionList.OrderBy(x => x.Order).ToList();
+                foreach (var item in orderedTemp)
+                {
+                    stringList.Add(String.Format("{0} - {1}", item.EditionName, item.Order));
+                }
+                orderedTemp.Clear();
+            }
+            return stringList;
+        }
+
+        
+    }
+}
---
+++ Week6/03.Static-Partial-Anonymous-Types/AnonymousTypesAndNestedClasses/TestApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypesAndNestedClasses
+{
+    class TestApp
+    {
+        static void Main(string[] args)
+        {
+            var books = new List<Book>();
+            books.Add(new Book() { Id = 2, Name = "Introduction to programming" });
+            books.Add(new Book() { Id = 5, Name = "Lord of the rings" });
+            books.Add(new Book() { Id = 14, Name = "Captain Nemo" });
+            books.Add(new Book() { Id = 8, Name = "Alice in wonderland" });
+            books.Add(new Book() { Id = 3, Name = "Spartak" });
+
+            var magazines = new List<Magazine>();
+            magazines.Add(new Magazine() { ISBN = 4, Title = "Optimetrix" });
+            magazines.Add(new Magazine() { ISBN = 7, Title = "Cars" });
+            magazines.Add(new Magazine() { ISBN = 3, Title = "Shopevery" });
+            magazines.Add(new Magazine() { ISBN = 9, Title = "Greeny" });
+            magazines.Add(new Magazine() { ISBN = 11, Title = "Youre home" });
+
+            var list = MagazineAndBookSorter.Sort(books, magazines);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
