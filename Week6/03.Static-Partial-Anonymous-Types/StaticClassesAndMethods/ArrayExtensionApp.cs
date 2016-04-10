b5a0524a91bd810db9fcfde7358f5a5e886121ee
Redsart <redsartvimes@yahoo.com>
2016-04-10 15:12:08 +0300

    Added Static-Partial-Anonymous-Types.

---
+++ Week6/03.Static-Partial-Anonymous-Types/StaticClassesAndMethods/ArrayExtensionApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassesAndMethods
+{
+    class ArrayExtensionApp
+    {
+        static void Main(string[] args)
+        {
+            var firstList = new List<int>() { 1, 3, 5, 7, 9 };
+            var secondList = new List<int>() { 2, 4, 6, 7, 9 };
+
+            var newList = ArrayExtension<int>.Union(firstList, secondList);
+            //var newList = ArrayExtension<int>.UnionAll(firstList, secondList);
+            //var newList = ArrayExtension<int>.Intersect(firstList, secondList);
+
+            foreach (var item in firstList)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(ArrayExtension<int>.GetReplacingValue());
+
+            var stringList = new List<string>() { "whisp", "elf", "orc", "human", "dwarf" };
+            Console.WriteLine(ArrayExtension<int>.Join(stringList));
+        }
+    }
+}
