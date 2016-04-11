bbb87ef86b85755d5dfa24e622109525287a1dd0
Redsart <redsartvimes@yahoo.com>
2016-04-11 13:45:13 +0300

    Added Partial classes and methods.

---
+++ Week6/03.Static-Partial-Anonymous-Types/PartialClassesAndMethods/EmployeePartial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassesAndMethods
+{
+    public partial class Employee
+    {
+        partial void Print()
+        {
+            Console.WriteLine("{0} {1}",this.FirstName,this.LastName);
+        }
+
+        public void CalculateAllIncome()
+        {
+            Console.WriteLine("{0:C2}",this.Salary+this.Bonus);
+        }
+
+        public void CalculateBalance()
+        {
+            var income = this.Salary + this.Bonus;
+            var procent = 20;
+            var incomeOutOfTaxes = income - (income * (procent / 100.0m));
+            Console.WriteLine("{0:C2} Tax rate={1}%",incomeOutOfTaxes,procent);
+        }
+    }
+}
---
+++ Week6/03.Static-Partial-Anonymous-Types/PartialClassesAndMethods/TestApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassesAndMethods
+{
+    class TestApp
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+            employee.FirstName = "Todor";
+            employee.LastName = "Kostov";
+            employee.Salary = 900;
+            employee.Bonus = 200;
+            employee.Position = "Sales Consultant";
+
+            employee.PrintNames();
+            employee.CalculateAllIncome();
+            employee.CalculateBalance();
+        }
+    }
+}
